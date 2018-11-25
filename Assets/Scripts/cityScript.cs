a1a12b6be97461c86d42a75825e28920261ce11e
Yi Fei Cheng <ycheng2@swarthmore.edu>
2018-11-25 15:12:45 -0500

    Successfully extruded buildings.

--- Assets/Scripts/cityScript.cs
+++ Assets/Scripts/cityScript.cs
@@ -12,6 +12,7 @@ public class cityScript : MonoBehaviour {
     // Road parameters 
     // Road width 
     public float roadwidth;
+    public float roadVariance;
 
     // Block parameters
     // Minimum dimensions  
@@ -24,6 +25,8 @@ public class cityScript : MonoBehaviour {
     public float minBuildingArea;
     public float minAllowableBuildingWidth;
     public float minAllowableBuildingLength;
+    public float minAllowableBuildingHeight;
+    public float maxAllowableBuildingHeight; 
 
     // Area calculation function 
     // Parameters: 
@@ -46,10 +49,13 @@ public class cityScript : MonoBehaviour {
     // GameObject originPlane: Plane for subdivision
     // bool Lengthwise division: Determines if length-wise or width-wise division
     private void subdivide(GameObject originPlane, bool lengthWiseDivision, bool includeRoad, float minBuildingBlockArea, 
-        float minAllowableWidth, float minAllowableLength)
+        float minAllowableWidth, float minAllowableLength, string name, string tag)
     {
         // Get vertices of original plane 
         Vector3[] originVert = originPlane.GetComponent<MeshFilter>().mesh.vertices;
+
+        originPlane.name = name;
+        originPlane.gameObject.tag = tag;
 
         // Calculate original area 
         float areaOrigin = calculateArea(originVert); 
@@ -82,7 +88,7 @@ public class cityScript : MonoBehaviour {
             float roadWidthWithNoise;
             if (includeRoad)
             {
-                roadWidthWithNoise = roadwidth + Random.Range(-0.5f, 0.5f);
+                roadWidthWithNoise = roadwidth + Random.Range(-roadVariance / 2.0f, roadVariance / 2.0f);
             } else
             {
                 roadWidthWithNoise = 0.0f;
@@ -93,8 +99,10 @@ public class cityScript : MonoBehaviour {
                 quadA = quadMesh(new Vector3[]{BL, BL + (BR - BL) * divisionPositionOne - (BR - BL).normalized * roadWidthWithNoise * 0.5f, TL, TL + (TR - TL) * divisionPositionTwo - (TR - TL).normalized * roadWidthWithNoise * 0.5f});
                 quadB = quadMesh(new Vector3[] { BL + (BR - BL) * divisionPositionOne + (BR - BL).normalized * roadWidthWithNoise * 0.5f, BR, TL + (TR - TL) * divisionPositionTwo + (TR - TL).normalized * roadWidthWithNoise * 0.5f, TR});
                 // Set names 
-                quadA.name = "BuildingBlock";
-                quadB.name = "BuildingBlock";
+                quadA.name = name;
+                quadB.name = name;
+                quadA.gameObject.tag = tag;
+                quadB.gameObject.tag = tag;
                 // Set parent object 
                 quadA.transform.SetParent(this.transform);
                 quadB.transform.SetParent(this.transform);
@@ -102,9 +110,9 @@ public class cityScript : MonoBehaviour {
                 DestroyImmediate(originPlane);
                 // Recursive call 
                 subdivide(quadA, !lengthWiseDivision, includeRoad, minBuildingBlockArea,
-                    minAllowableWidth, minAllowableLength);
+                    minAllowableWidth, minAllowableLength, name, tag);
                 subdivide(quadB, !lengthWiseDivision, includeRoad, minBuildingBlockArea,
-                    minAllowableWidth, minAllowableLength);
+                    minAllowableWidth, minAllowableLength, name, tag);
             }
             // Width-wise division
             else if (!lengthWiseDivision && minLength > minAllowableLength)
@@ -113,8 +121,10 @@ public class cityScript : MonoBehaviour {
                 quadA = quadMesh(new Vector3[]{BL, BR, BL + (TL-BL)*divisionPositionOne - (TL - BL).normalized * roadWidthWithNoise * 0.5f, BR + (TR - BR) * divisionPositionTwo - (TR - BR).normalized * roadWidthWithNoise * 0.5f });
                 quadB = quadMesh(new Vector3[]{BL + (TL - BL) * divisionPositionOne + (TL - BL).normalized * roadWidthWithNoise * 0.5f, BR + (TR - BR) * divisionPositionTwo + (TR - BR).normalized * roadWidthWithNoise * 0.5f, TL, TR});
                 // Set names 
-                quadA.name = "BuildingBlock";
-                quadB.name = "BuildingBlock";
+                quadA.name = name;
+                quadB.name = name;
+                quadA.gameObject.tag = tag;
+                quadB.gameObject.tag = tag;
                 // Set parent object 
                 quadA.transform.SetParent(this.transform);
                 quadB.transform.SetParent(this.transform);
@@ -122,14 +132,14 @@ public class cityScript : MonoBehaviour {
                 DestroyImmediate(originPlane);
                 // Recursive call 
                 subdivide(quadA, !lengthWiseDivision, includeRoad, minBuildingBlockArea,
-                    minAllowableWidth, minAllowableLength);
+                    minAllowableWidth, minAllowableLength, name, tag);
                 subdivide(quadB, !lengthWiseDivision, includeRoad, minBuildingBlockArea,
-                    minAllowableWidth, minAllowableLength);
+                    minAllowableWidth, minAllowableLength, name, tag);
             }
             else
             {
                 subdivide(originPlane, !lengthWiseDivision, includeRoad, minBuildingBlockArea,
-                    minAllowableWidth, minAllowableLength);
+                    minAllowableWidth, minAllowableLength, name, tag);
             }
             
 
@@ -236,6 +246,7 @@ public class cityScript : MonoBehaviour {
         // Generate first buildingBlock 
         GameObject buildingBlock = quadMesh(groundPlaneVertices);
         buildingBlock.name = "BuildingBlock";
+        buildingBlock.gameObject.tag = "BuildingBlock";
         buildingBlock.transform.SetParent(this.transform);
 
         // Subdivide buildingBlock 
@@ -244,29 +255,224 @@ public class cityScript : MonoBehaviour {
         // First recursive call to subdivide 
         // Generate road map 
         subdivide(buildingBlock, lengthWiseDivision, true, minBlockArea,
-                    minAllowableBlockWidth, minAllowableBlockLength);
-        // Make further divisions to initially generated plots
-        // For actual building parameters in a block
-        /*
-        foreach(GameObject aBuildingBlock in GameObject.FindGameObjectsWithTag("Untagged"))
-        {
-            if(aBuildingBlock.name == "BuildingBlock")
-            {
-                subdivide(aBuildingBlock, lengthWiseDivision, false, minBuildingArea,
-                    minAllowableBuildingWidth, minAllowableBuildingLength);
-            }
-        }
-        */
+                    minAllowableBlockWidth, minAllowableBlockLength, "BuildingBlock", "BuildingBlock");
         // Disable ground plane visibility 
         groundPlane.SetActive(false);
     }
 
+    // Function for generating building lots from roadmap 
+    private void generateBuildingLots()
+    {
+        // Make further divisions to initially generated plots
+        // For actual building parameters in a block
+        foreach (GameObject aBuildingBlock in GameObject.FindGameObjectsWithTag("BuildingBlock"))
+        {
+            bool lengthWiseDivision = Random.value > 0.5f;
+            subdivide(aBuildingBlock, lengthWiseDivision, false, minBuildingArea,
+                    minAllowableBuildingWidth, minAllowableBuildingLength, "BuildingLot", "BuildingLot");
+        }
+    }
+
+    // Function for extruding a single building from a building lot
+    private void extrudeBuilding(float height, GameObject buildingLot)
+    {
+        // Instantiate a new GameObject newBuilding
+        GameObject newBuilding = new GameObject();
+
+        // Add mesh components to GameObject
+        newBuilding.AddComponent<MeshFilter>();
+        newBuilding.AddComponent<MeshRenderer>();
+        newBuilding.AddComponent<MeshCollider>();
+
+        // Generate mesh for newQuad 
+        Mesh newBuildingMesh = new Mesh();
+
+        // Get vertex geometry of buildingLot 
+        Vector3[] lotVertices = buildingLot.GetComponent<MeshFilter>().mesh.vertices;
+
+        // Height vector 
+        Vector3 heightVec = new Vector3(0.0f, height, 0.0f);
+
+        // Define new vertex geometry 
+        Vector3[] buildingVertices =
+        {
+            // Bottom lower left corner
+            lotVertices[0],
+            lotVertices[0],
+            lotVertices[0],
+            // Bottom lower right color 
+            lotVertices[1],
+            lotVertices[1],
+            lotVertices[1],
+            // Bottom upper left corner 
+            lotVertices[2],
+            lotVertices[2],
+            lotVertices[2],
+            // Bottom upper right corner 
+            lotVertices[3],
+            lotVertices[3],
+            lotVertices[3],
+            // Top lower left corner
+            lotVertices[0] + heightVec,
+            lotVertices[0] + heightVec,
+            lotVertices[0] + heightVec,
+            // Top lower right corner 
+            lotVertices[1] + heightVec,
+            lotVertices[1] + heightVec,
+            lotVertices[1] + heightVec,
+            // Top upper left corner 
+            lotVertices[2] + heightVec,
+            lotVertices[2] + heightVec,
+            lotVertices[2] + heightVec,
+            // Top upper right corner
+            lotVertices[3] + heightVec,
+            lotVertices[3] + heightVec,
+            lotVertices[3] + heightVec,
+
+        };
+        // Set newBuildingMesh vertices 
+        newBuildingMesh.vertices = buildingVertices;
+
+        // Define new triangles 
+        int[] triangles =
+        {
+            // Face front 
+            0, 12, 3, 
+            12, 15, 3,
+            // Face right 
+            4, 16, 9, 
+            16, 21, 9,
+            // Face back 
+            10, 22, 6, 
+            22, 18, 6,
+            // Face left 
+            7, 19, 1,
+            19, 13, 1,
+            // Face bottom 
+            8, 2, 11, 
+            2, 5, 11, 
+            // Face top 
+            14, 20, 17,
+            20, 23, 17
+        };
+        // Set newBuildingMesh triangles 
+        newBuildingMesh.triangles = triangles;
+
+        // Assign uv values for texturing 
+        Vector2[] uv = new Vector2[24];
+        // Front 
+        uv[0] = new Vector2(0, 0);
+        uv[12] = new Vector2(0, 1);
+        uv[3] = new Vector2(1, 0);
+        uv[15] = new Vector2(1, 1);
+        // Right
+        uv[4] = new Vector2(0, 0);
+        uv[16] = new Vector2(0, 1);
+        uv[9] = new Vector2(1, 0);
+        uv[21] = new Vector2(1, 1);
+        // Back 
+        uv[10] = new Vector2(0, 0);
+        uv[22] = new Vector2(0, 1);
+        uv[6] = new Vector2(1, 0);
+        uv[18] = new Vector2(1, 1);
+        // Left 
+        uv[7] = new Vector2(0, 0);
+        uv[19] = new Vector2(0, 1);
+        uv[1] = new Vector2(1, 0);
+        uv[13] = new Vector2(1, 1);
+        // Bottom 
+        uv[8] = new Vector2(0, 0);
+        uv[2] = new Vector2(0, 1);
+        uv[11] = new Vector2(1, 0);
+        uv[5] = new Vector2(1, 1);
+        // Top 
+        uv[14] = new Vector2(0, 0);
+        uv[20] = new Vector2(0, 1);
+        uv[17] = new Vector2(1, 0);
+        uv[23] = new Vector2(1, 1);
+
+        newBuildingMesh.uv = uv; 
+
+        // Calculate normals 
+        Vector3[] normals = new Vector3[24];
+        // Front normal 
+        Vector3 frontNormal = Vector3.Cross((buildingVertices[12] - buildingVertices[0]), (buildingVertices[12] - buildingVertices[3]));
+        frontNormal.Normalize();
+        normals[0] = frontNormal;
+        normals[12] = frontNormal;
+        normals[3] = frontNormal;
+        normals[15] = frontNormal;
+        // Right normal 
+        Vector3 rightNormal = Vector3.Cross((buildingVertices[16] - buildingVertices[4]), (buildingVertices[9] - buildingVertices[4]));
+        rightNormal.Normalize();
+        normals[4] = rightNormal;
+        normals[16] = rightNormal;
+        normals[9] = rightNormal;
+        normals[21] = rightNormal;
+        // Back normal 
+        Vector3 backNormal = Vector3.Cross((buildingVertices[22] - buildingVertices[10]), (buildingVertices[6] - buildingVertices[10]));
+        backNormal.Normalize();
+        normals[10] = backNormal;
+        normals[22] = backNormal;
+        normals[6] = backNormal;
+        normals[18] = backNormal;
+        // Left normal 
+        Vector3 leftNormal = Vector3.Cross((buildingVertices[19] - buildingVertices[7]), (buildingVertices[1] - buildingVertices[7]));
+        leftNormal.Normalize();
+        normals[7] = leftNormal;
+        normals[19] = leftNormal;
+        normals[1] = leftNormal;
+        normals[13] = leftNormal;
+        // Bottom normal 
+        Vector3 bottomNormal = Vector3.Cross((buildingVertices[2] - buildingVertices[8]), (buildingVertices[11] - buildingVertices[8]));
+        bottomNormal.Normalize();
+        normals[2] = bottomNormal;
+        normals[8] = bottomNormal;
+        normals[11] = bottomNormal;
+        normals[5] = bottomNormal;
+        //Top normal
+        Vector3 topNormal = Vector3.Cross((buildingVertices[20] - buildingVertices[14]), (buildingVertices[17] - buildingVertices[14]));
+        topNormal.Normalize();
+        normals[14] = topNormal;
+        normals[20] = topNormal;
+        normals[17] = topNormal;
+        normals[23] = topNormal;
+        // Set newBuildingMesh normals
+        newBuildingMesh.normals = normals;
+
+        // Set mesh in newBuidling GameObject
+        // Set mesh for MeshFilter
+        newBuilding.GetComponent<MeshFilter>().mesh = newBuildingMesh;
+        // Set mesh for MeshCollider 
+        newBuilding.GetComponent<MeshCollider>().sharedMesh = newBuildingMesh;
+        // Set default material 
+        newBuilding.GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Diffuse"));
+
+        // Set parent of building 
+        newBuilding.transform.SetParent(buildingLot.transform);
+        // Set name and tag 
+        newBuilding.name = "Building";
+        newBuilding.gameObject.tag = "Building";
+    }
+
+    // Function to extrude all buildings from building lots 
+    private void extrudeAll(float minHeight, float maxHeight)
+    {
+        foreach (GameObject aBuildingLot in GameObject.FindGameObjectsWithTag("BuildingLot"))
+        {
+            float buildingHeight = Random.Range(minHeight, maxHeight);
+            extrudeBuilding(buildingHeight, aBuildingLot);
+        }
+    }
 
 	// Use this for initialization
 	void Start () {
         // Generate roadmap 
         generateRoadMap();
-
+        // Subdivide blocks into building lots 
+        generateBuildingLots();
+        // Extrude buildings from building block
+        extrudeAll(minAllowableBuildingHeight, maxAllowableBuildingHeight);
     }
 	
 	// Update is called once per frame
