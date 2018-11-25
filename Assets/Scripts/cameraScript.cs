27beb7fee2ad6185a2031274ceeb4db7159d5533
Yi Fei Cheng <ycheng2@swarthmore.edu>
2018-11-25 15:35:18 -0500

    Added camera script for movement.

---
+++ Assets/Scripts/cameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(new Vector3(0.0f, 0.0f, 0.0f));
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+}
